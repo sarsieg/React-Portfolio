fa412488d858e7627c6cf77a3a9b227b4e16841d
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-16 13:15:57 -0500

    adding code into service-worker.js file

--- react-portfolio/src/serviceWorker.js
+++ react-portfolio/src/serviceWorker.js
@@ -1,3 +1,33 @@
-import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import data from '../data';
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+export function register(config) {
+    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+        if (publicUrl.origin !== window.location.origin) {
+            return;
+        }
+
+        window.addEventListener('load', () => {
+            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+            if (isLocalhost) {
+                checkValidServiceWorker(swUrl, config);
+
+                navigator.serviceWorker.ready.then(() => {
+                    console.log(
+                        'This web app is being served cache-first by a service ' + 'worker. To learn more, visit https://bit.ly/CR-PWA'
+                    );
+                });
+            } else {
+                registerValidSW(swUrl, config);
+            }
+        });
+    }
+}
+
