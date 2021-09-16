8668581db8774a64f938fddd31b4dc1481a55972
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-16 13:24:59 -0500

    finished writing in code for serviceworker.js file

--- react-portfolio/src/serviceWorker.js
+++ react-portfolio/src/serviceWorker.js
@@ -31,3 +31,69 @@ export function register(config) {
     }
 }
 
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+        registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
+            if (installingWorker === null) {
+                return;
+            }
+            installingWorker.onstatechange = () => {
+                if (installingWorker.state === 'installed') {
+                    if (navigator.serviceWorker.controller) {
+                        console.log(
+                        'New content is available and will be used when all ' + 'tabs for this page are closed. See https://bit.ly/CRA-PWA. '
+                        );
+
+                        if (config && config.onUpdate) {
+                            config.onUpdate(registration);
+                        }
+                    } else {
+                        console.log ('Content is cached for offline use.');
+
+                        if (config && config.onSuccess) {
+                            config.onSuccess(registration);
+                        }
+                    }
+                }
+            };
+        };
+    })
+    .catch(error => {
+        console.error('Error during service worker registration:', error);
+    });
+}
+
+function checkValidServiceWorker(swUrl, config) {
+    fetch(swUrl)
+    .then(response => {
+        const contentType = response.headers.get('content-type');
+        if (
+            response.status === 404 ||
+            (contentType != null && contentType.indexOf('javascript') === -1)
+        ) {
+            navigator.serviceWorker.ready.then(registration => {
+                registration.unregister().then(() => {
+                    window.location.reload();
+                });
+            });
+        } else {
+            registerValidSW(swUrl, config);
+        }
+    })
+    .catch(() => {
+        console.log(
+            'No internet connection found. App is running in offline mode.'
+        );
+    });
+}
+
+export function unregister() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then(registration => {
+            registration.unregister();
+        });
+    }
+}
