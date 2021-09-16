8d42ab2db1cf000b576e57cd77f3633f9a7a45e2
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-15 23:23:38 -0500

    imported react, fade and data links into header.js file and added buttons that link to my email as well as resume

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,3 +1,35 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../data';
+
+class Header extends Component {
+    state = {}
+
+    render() {
+        return ( < div >
+            <
+            h1 className = 'heading-background' > S SIEGEL < /h1> <
+            header >
+            <
+            h1 >
+            <
+            Fade bottom cascade > { data.name } < /Fade> <
+            /h1> <
+            /header> <
+            Fade bottom >
+            <
+            p className = 'header-title' > { data.headerTagline[0] } < br > < /br>{data.headerTagline[1]}<br></br > { data.headerTagline[2] } <
+            br > < /br> <
+            button > < a href = { `mailto:${data.contactEmail}` }
+            rel = "noopener noreferrer" > Contact < /a></button >
+            <
+            button > < a href = { `${data.resume}` }
+            rel = "noopener noreferrer" > Resume < /a></button >
+            <
+            /p> <
+            /Fade> <
+            /div>);
+        }
+    }
+
+    export default Header;
