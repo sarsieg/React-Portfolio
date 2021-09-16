81c6a8758c44d716d846e10f005d594ca97e5eb7
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-16 00:04:49 -0500

    imported react and link for an animated scroll as well as added link to home, about me and work sections. finally exported default navbar;

---
+++ react-portfolio/src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
+
+class Navbar extends Component {
+    state = {  }
+    scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+    render() {
+        return (<nav>
+            <ul>
+                <li><Link
+                className='link'
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={this.scrollToTop}
+                >Home</Link></li>
+
+                <li><Link
+                className='link'
+                activeClass="active"
+                to="About"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={this.scrollToTop}
+                >About</Link></li>
+                
+                <li><Link
+                className='link'
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={this.scrollToTop}
+                >Contact</Link></li>
+                
+            </ul>
+        </nav>);
+    }
+}
+
+export default Navbar;
