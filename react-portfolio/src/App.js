646c14d68c9eb98ec72d4da3b9284349f33576ed
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-16 13:36:15 -0500

    fixed code in app.test.js file

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/work';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Header></Header>
+        </div>
+
+        <div className='About'>
+          <About></About>
+        </div>
+        <div className='work'>
+          <Work></Work>
+        </div>
+        <div className='contact'>
+          <Contact></Contact>
+        </div>
+        </div>
   );
 }
 
