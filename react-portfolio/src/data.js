064319b2e1f8cfc3ec1e4be8785ae0a1aff14cf9
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-15 22:07:26 -0500

    added data.js file as well as basic layout for the document. adding in 6 different projects with different ids for each

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,59 @@
+export default {
+
+    name: 'Sarah Siegel',
+    headerTagline: [
+        'I am Sarah Siegel',
+        'a full stack web developer',
+        'from Minneapolis Minnesota.'
+    ],
+
+    contactEmail: 'sarah.ac.siegel@gmail.com',
+    resume: '',
+    abouttext: "I am a fast learner and a hard worker who is passonate about developing full stack web programs that are not only visually appealing but are also fully functional applications.",
+    aboutImage: '',
+    ShowAboutImage: true,
+
+    projects: [{
+            id: 1,
+            title: 'Food Finders',
+            service: 'Helping you choose your next meal',
+            imageSrc: '',
+            url: 'https://claholla.github.io/Food-Finders/'
+        },
+        {
+            id: 2,
+            title: 'Weather App',
+            service: 'Application showing you weather updates and forecasts',
+            imageSrc: '',
+            url: 'https://sarsieg.github.io/Weather-Dashboard/'
+        },
+        {
+            id: 3,
+            title: 'Food Finders',
+            service: 'Helping you choose your next meal',
+            imageSrc: '',
+            url: 'https://claholla.github.io/Food-Finders/'
+        },
+        {
+            id: 4,
+            title: 'Food Finders',
+            service: 'Helping you choose your next meal',
+            imageSrc: '',
+            url: 'https://claholla.github.io/Food-Finders/'
+        },
+        {
+            id: 5,
+            title: 'Food Finders',
+            service: 'Helping you choose your next meal',
+            imageSrc: '',
+            url: 'https://claholla.github.io/Food-Finders/'
+        },
+        {
+            id: 6,
+            title: 'Food Finders',
+            service: 'Helping you choose your next meal',
+            imageSrc: '',
+            url: 'https://claholla.github.io/Food-Finders/'
+        },
+    ]
+}
