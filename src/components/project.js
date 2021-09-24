36f6a28aa2fd78553c1ad32494e592b65f68e14f
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-23 20:02:38 -0500

    final push

--- src/components/project.js
+++ src/components/project.js
@@ -8,6 +8,7 @@ class Project extends Component {
             <Fade bottom>
                 <div className='project'>
                     <h1>{this.props.title}</h1>
+                    
                     <a href={this.props.url}>
                         <img src={this.props.imageSrc} alt={this.props.title}></img>
                     </a>
